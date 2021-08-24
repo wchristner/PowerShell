3e1fcce20f6b76a0c070e66157232682c693f8ad
William Christner <wchristner@gmail.com>
2021-08-24 00:43:16 -0400

    Add files via upload
    Funny IT Excuse Generator

---
+++ IT excuse generator.ps1
@@ -0,0 +1,13 @@
+﻿function Get-Excuse
+{
+  $url = 'http://pages.cs.wisc.edu/~ballard/bofh/bofhserver.pl'
+  $ProgressPreference = 'SilentlyContinue'
+  $page = Invoke-WebRequest -Uri $url -UseBasicParsing
+  $pattern = '<br><font size = "\+2">(.+)'
+
+  if ($page.Content -match $pattern)
+  {
+    $matches[1]
+  }
+}
+Get-Excuse
